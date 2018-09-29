7fb2f9bcd14dd9c04e8f3150b386d096038eec30
Julian Salinas <julian4001@outlook.com>
2018-09-28 20:06:35 -0600

    Más imágenes de aulas

--- app/src/main/java/edepa/custom/WallpaperLocation.java
+++ app/src/main/java/edepa/custom/WallpaperLocation.java
@@ -32,14 +32,32 @@ public enum WallpaperLocation {
     EDIFICIO_F ("((aula)|(edifi?c?i?o?)|(audito?r?i?o))\\s+f2?",
             R.drawable.tec_edificio_f2),
 
+    EDIFICIO_B3 ("((aula)|(edifi?c?i?o?)|(audito?r?i?o))\\s+b3?",
+            R.drawable.tec_edificio_b3),
+
+    EDIFICIO_ELECTRONICA ("((aula)|(edifi?c?i?o?)|(audito?r?i?o))?((\\w|\\s)+)?electron?i?c?a?",
+            R.drawable.tec_edificio_electronica),
+
     LAIMI_1 ("((labo?r?a?t?o?r?i?o?)|(laimi?))\\s*1",
             R.drawable.tec_laimi_1),
 
     LAIMI_2 ("((labo?r?a?t?o?r?i?o?)|(laimi?))\\s*2",
             R.drawable.tec_laimi_2),
 
+    LAB_H_AZUL("(((labo?r?a?t?o?r?i?o?)|(laimi?))\\s+h)(\\w|\\s)(rojo)",
+            R.drawable.tec_laboratorio_h_rojo),
+
+    LAB_H_ROJO("(((labo?r?a?t?o?r?i?o?)|(laimi?))\\s+h)(\\w|\\s)(azul)",
+            R.drawable.tec_laboratorio_h_azul),
+
     LAB_H("(((labo?r?a?t?o?r?i?o?)|(laimi?))\\s+h)",
             R.drawable.tec_laboratorio_h),
+
+    LAB_208("((aula)|(edifi?c?i?o?)|(audito?r?i?o)|(labo?r?a?t?o?r?i?o?)|(laimi?))\\s+208",
+            R.drawable.tec_laboratorio_208),
+
+    LAB_213("((aula)|(edifi?c?i?o?)|(audito?r?i?o)|(labo?r?a?t?o?r?i?o?)|(laimi?))\\s+213",
+            R.drawable.tec_laboratorio_213),
 
     TIERRA_MEDIA("(((labo?r?a?t?o?r?i?o)|(laimi?))\\s+b3\\s*10?)|((tierr\\w+)|(medi\\w+))",
             R.drawable.tec_tierra_media),
