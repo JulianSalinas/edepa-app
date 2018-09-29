324a63a003bca2591798a4a001fae069dbe9154a
Julian Salinas <julian4001@outlook.com>
2018-09-29 12:20:04 -0600

    #Bugfix Ciertos lugares con imagen incorrecta

--- app/src/main/java/edepa/custom/WallpaperLocation.java
+++ app/src/main/java/edepa/custom/WallpaperLocation.java
@@ -8,11 +8,32 @@ public enum WallpaperLocation {
             "((biblio?t?e?c?a?)|(figueres)|(ferrer))",
             R.drawable.tec_biblioteca),
 
+    TIERRA_MEDIA("(((labo?r?a?t?o?r?i?o?)|(aula)|(laimi?))\\s+b3(\\w|\\s|-)*10?)|((tierr\\w+)|(medi\\w+))",
+            R.drawable.tec_tierra_media),
+
+    LAB_208("((aula)|(edifi?c?i?o?)|(audito?r?i?o)|(labo?r?a?t?o?r?i?o?)|(laimi?))\\s+208",
+            R.drawable.tec_laboratorio_208),
+
+    LAB_213("((aula)|(edifi?c?i?o?)|(audito?r?i?o)|(labo?r?a?t?o?r?i?o?)|(laimi?))\\s+213",
+            R.drawable.tec_laboratorio_213),
+
+    LAB_H_AZUL("(((labo?r?a?t?o?r?i?o?)|(laimi?))\\s+h)(\\w|\\s)+(rojo)",
+            R.drawable.tec_laboratorio_h_rojo),
+
+    LAB_H_ROJO("(((labo?r?a?t?o?r?i?o?)|(laimi?))\\s+h)(\\w|\\s)+(azul)",
+            R.drawable.tec_laboratorio_h_azul),
+
+    LAB_H("(((labo?r?a?t?o?r?i?o?)|(laimi?))\\s+h)",
+            R.drawable.tec_laboratorio_h),
+
     CENTRO_ARTES ("(centro)?(\\s|\\w|d)*artes?",
             R.drawable.tec_centro_artes),
 
     CIC ("((cic)|(centro(\\w|\\s)+investigacion(\\w|\\s)+computacion))",
             R.drawable.tec_edificio_a5),
+
+    EDIFICIO_ELECTRONICA ("((aula)|(edifi?c?i?o?)|(audito?r?i?o)|(labo?r?a?t?o?r?i?o?))?((\\w|\\s)+)?electron?i?c?a?",
+            R.drawable.tec_edificio_electronica),
 
     EDIFICIO_A4 (
             "(((aula)|(edifi?c?i?o?)|(audito?r?i?o)|(labo?r?a?t?o?r?i?o?))\\s+a4)|((escuela)?((\\w|\\s)+)?matem?a?t?i?c?a?)",
@@ -36,32 +57,11 @@ public enum WallpaperLocation {
     EDIFICIO_B3 ("((aula)|(edifi?c?i?o?)|(audito?r?i?o)|(labo?r?a?t?o?r?i?o?))\\s+b3?",
             R.drawable.tec_edificio_b3),
 
-    EDIFICIO_ELECTRONICA ("((aula)|(edifi?c?i?o?)|(audito?r?i?o)|(labo?r?a?t?o?r?i?o?))?((\\w|\\s)+)?electron?i?c?a?",
-            R.drawable.tec_edificio_electronica),
-
     LAIMI_1 ("((labo?r?a?t?o?r?i?o?)|(laimi?))\\s*1",
             R.drawable.tec_laimi_1),
 
     LAIMI_2 ("((labo?r?a?t?o?r?i?o?)|(laimi?))\\s*2",
             R.drawable.tec_laimi_2),
-
-    LAB_H_AZUL("(((labo?r?a?t?o?r?i?o?)|(laimi?))\\s+h)(\\w|\\s)(rojo)",
-            R.drawable.tec_laboratorio_h_rojo),
-
-    LAB_H_ROJO("(((labo?r?a?t?o?r?i?o?)|(laimi?))\\s+h)(\\w|\\s)(azul)",
-            R.drawable.tec_laboratorio_h_azul),
-
-    LAB_H("(((labo?r?a?t?o?r?i?o?)|(laimi?))\\s+h)",
-            R.drawable.tec_laboratorio_h),
-
-    LAB_208("((aula)|(edifi?c?i?o?)|(audito?r?i?o)|(labo?r?a?t?o?r?i?o?)|(laimi?))\\s+208",
-            R.drawable.tec_laboratorio_208),
-
-    LAB_213("((aula)|(edifi?c?i?o?)|(audito?r?i?o)|(labo?r?a?t?o?r?i?o?)|(laimi?))\\s+213",
-            R.drawable.tec_laboratorio_213),
-
-    TIERRA_MEDIA("(((labo?r?a?t?o?r?i?o)|(laimi?))\\s+b3\\s*10?)|((tierr\\w+)|(medi\\w+))",
-            R.drawable.tec_tierra_media),
 
     TEC_SEDE_CARTAGO("(tecn?o?l?o?g?i?c?o?[\\s+]?)|(sede((\\w|\\s)+)?cartago)",
             R.drawable.tec_sede_cartago);
